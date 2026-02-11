43f9611b5ae815f96fcfbba392f77a640793f073
itz-les3 <markleslie079@gmail.com>
2026-02-11 10:41:28 +0300

    input

---
+++ python-basics/numbers.py
@@ -0,0 +1,20 @@
+# Name: Mark Leslie
+# Date: 11/02/2026
+
+# Program to add numbers, subtract and multiply 2 numbers from input
+
+first_number =int(input("Enter the first number: "))
+second_number =int(input("Enter the second number: "))
+
+sum_numbers = first_number + second_number
+difference_numbers = first_number - second_number
+product_numbers = first_number * second_number
+quotient_numbers = first_number / second_number
+
+
+
+
+print(f"The sum of the numbers: {sum_numbers}")
+print(f"The difference of the numbers: {difference_numbers}")
+print(f"The product of the numbers: {product_numbers}")
+print(f"The quotient of the numbers: {quotient_numbers}")
---
+++ python-basics/user_input.py
@@ -0,0 +1,8 @@
+#Name:Mark Leslie
+#Date:11/02/2026
+
+#Program to get user input
+
+name = input("What is your name? ")
+
+print(name)
