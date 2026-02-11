5d292b502a3222bdd31874d04da8b6e46f664035
itz-les3 <markleslie079@gmail.com>
2026-02-11 09:18:27 +0300

    first commit

---
+++ python-basics/challenge 1.py
@@ -0,0 +1,9 @@
+university = ""
+grade = ""
+favourite_course = ""
+cluster_points = 69
+
+print(f"My favourite university is {university}")
+print(f"My favourite course is {favourite_course}")
+print(f"My cluster points are {cluster_points}")
+print(f"My grade is {grade}")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# this comment
+print("Hello from Leslie")
+# city/town
+print("I am from K.Road")
+#my hobbies
+print("I like playing football")
+
+age= 26
+height= 1.65
+weight= 60
+
+print(f" I am {age} years old")
+print(f" I am also {height} meters tall")
+print(f" and weigh {weight} kgst")
