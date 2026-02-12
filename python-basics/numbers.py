3b81254ddd97860550b78751bea8ce3be8ed9dfa
itz-les3 <markleslie079@gmail.com>
2026-02-12 10:39:24 +0300

    strinh formattion

--- python-basics/numbers.py
+++ python-basics/numbers.py
@@ -3,18 +3,27 @@
 
 # Program to add numbers, subtract and multiply 2 numbers from input
 
-first_number =int(input("Enter the first number: "))
-second_number =int(input("Enter the second number: "))
+#first_number =int(input("Enter the first number: "))
+#second_number =int(input("Enter the second number: "))
 
-sum_numbers = first_number + second_number
-difference_numbers = first_number - second_number
-product_numbers = first_number * second_number
-quotient_numbers = first_number / second_number
-
-
+#sum_numbers = first_number + second_number
+#difference_numbers = first_number - second_number
+#product_numbers = first_number * second_number
+#quotient_numbers = first_number / second_number
 
 
-print(f"The sum of the numbers: {sum_numbers}")
-print(f"The difference of the numbers: {difference_numbers}")
-print(f"The product of the numbers: {product_numbers}")
-print(f"The quotient of the numbers: {quotient_numbers}")
+
+
+#print(f"The sum of the numbers: {sum_numbers}")
+#print(f"The difference of the numbers: {difference_numbers}")
+#print(f"The product of the numbers: {product_numbers}")
+#print(f"The quotient of the numbers: {quotient_numbers}")
+
+Third_number = input("what is third number")
+
+print(f"The type of third number is: {type(Third_number)}")
+
+#convert third number to an integer
+Third_number = int(Third_number)
+
+print(f"The type of third number is: {type(Third_number)}")
---
+++ python-basics/string_formatting.py
@@ -0,0 +1,42 @@
+# Name: Mark Leslie
+# Date: 11/02/2026
+#string formatting
+
+#Get string  length
+sentence = "I am learning Python"
+
+string_length = len(sentence)
+
+print(f"The length is:{string_length}")
+
+#splitting  a string 
+sentence_2 ="Mathematics Physics"
+split=sentence_2.split(" ")
+
+print(f"the first subject is: {split[0]}")
+
+
+#Make everthing CAPS
+mpesa_code ="us24htfhfn"
+
+capitalized = mpesa_code.upper()
+
+print("New mpesa code : ", capitalized)
+#Make everything small
+mpesa_code_2 = "US24HTHFNFN"
+small = mpesa_code_2.lower()
+print("New mpesa code : ", small)
+
+#Replacing characters in a string
+
+balance = "100Kshs"
+amount_added = "50Kshs"
+
+cleaned_balance = balance.replace("Kshs", "")
+
+print(f"Cleaned balance: ",cleaned_balance)
+ # asssignment
+ Mpesa_message = "CONFIRMED you have received 40Kshs from John Doe"
+print(Mpesa_message)
+split1= Mpesa_message.split(" ")
+print(f"The amount received is: {split1[5]}")
